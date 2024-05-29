1a40205102a71fdbba4268f71a1e4419411c2f30
hammemi nour allah <hammemi.nourallah@gmail.com>
2024-05-29 15:41:35 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+
+namespace mysqlapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Get connection string
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            // Connect to database and retrieve data
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM `utilisateur`";
+                using (var cmd = new MySqlCommand(sql, connection))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Assuming you have columns named 'id' and 'name' in your table
+                            Console.WriteLine($"ID: {reader["Id"]}, Name: {reader["nom"]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
